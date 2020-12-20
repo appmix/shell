7b6069b538a16cf4515de9b7abbed1ca34c59abf
ro <ro@ro>
2020-12-20 16:10:08 +0200

    str_replace.cpp

---
+++ str_replace.cpp
@@ -0,0 +1,10 @@
+std::string replaceFirstOccurrence(
+    std::string& s,
+    const std::string& toReplace,
+    const std::string& replaceWith)
+{
+    std::size_t pos = s.find(toReplace);
+    if (pos == std::string::npos) return s;
+    return s.replace(pos, toReplace.length(), replaceWith);
+}
+
